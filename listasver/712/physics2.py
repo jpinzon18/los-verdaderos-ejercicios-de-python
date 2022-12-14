ec0c1c85e259a20dc731f15283fc97912e5be8c2
unknown <APRENDIZ@SOAAPRCIDFDS007.FormacionSena.RED>
2022-12-14 07:56:06 -0500

    correccion del 14 de diciembre

---
+++ listasver/712/physics2.py
@@ -0,0 +1,11 @@
+#realice un programa que reciba del usuario una unidad de en temeperatura (celcius) y lo convierta a energia electrica(watts)
+def med ():
+    medi=input('introduzca la unidad de  medida: ')
+    print('la cantidad de grados celcius ingresada es: ',medi  )
+    operation=(medi*1.16194)
+    print('la conversion de celcius a watts es: ',operation)    
+   
+    
+        
+        
+med()        
