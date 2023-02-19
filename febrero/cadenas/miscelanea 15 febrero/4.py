96e8574b54d3c5b5f7bfda92136307c3c65442e2
jpinzon18 <116192630+jpinzon18@users.noreply.github.com>
2023-02-19 17:57:40 -0500

    Add files via upload

---
+++ febrero/cadenas/miscelanea 15 febrero/4.py
@@ -0,0 +1,32 @@
+menu1=print("presione 1 para pasar la cadena de minuscula a mayuscula")
+menu2=print("presione 2 para pasar la cadena de mayuscula a minuscula")
+menu3=print("presione 3 para que solo el primer elemento de la cadena pase a mayuscula")
+try:
+ number=(int(input("presione el numero correspondiente a la opcion que desea usar: ")))
+except ValueError:
+    print("el dato ingresado no esta soportado reinicie el programa")
+    
+def mayus():
+    cadena1=(input("ingrese los elementos de la cadena : "))
+    thecadena_upper=cadena1.upper()
+    print("la palabra pasada a mayusculas es:",thecadena_upper)
+
+def minus():
+    cadena2=(input("ingrese los elementos de la cadena: "))
+    cadena2_lower=cadena2.lower()
+    print("La palabra pasada a minusculas es:",cadena2_lower)
+
+def firstmayus():
+    cadena3=(input("ingrese los elementos de la cadena: "))
+    cadena3_capitalize=cadena3.capitalize()
+    print("la primera letra de la cadena pasada a mayusculas es:",cadena3_capitalize)
+
+try:
+ if number==1:
+     mayus()
+ elif number==2:
+     minus()
+ if number==3:
+     firstmayus()
+except NameError:
+    print("")    
---
+++ febrero/cadenas/miscelanea 15 febrero/6.py
@@ -0,0 +1,28 @@
+def timeverb():
+    verbo=(input("ingrese el verbo:"))
+    if verbo.endswith("ar"):
+        print("la palabra esta en presente simple")
+    elif verbo.endswith("er"):
+        print("la palabra esta en presente simple")
+    if verbo.endswith("ir"):
+        print("la palabra esta en presente simple")
+    elif verbo.endswith("es"):
+        print("la palabra esta en presente simple")
+    if verbo.endswith("e"):
+        print("la palabra esta en pasado simple")
+    elif verbo.endswith("i"):
+        print("la palabra esta en pasado simple")
+    if verbo.endswith("o"):
+        print("la palabra esta en pasado simple")
+    elif verbo.endswith("is"):
+        print("la palabra esta en pasado simple")
+    if verbo.endswith("ra"):
+        print("la palabra esta en futuro simple")
+    elif verbo.endswith("re"):
+        print("la palabra esta en futuro simple")
+    if verbo.endswith("os"):
+        print("la palabra esta en futuro simple")
+    elif verbo.endswith("á"):
+        print("la palabra esta en futuro simple")
+   
+timeverb()
