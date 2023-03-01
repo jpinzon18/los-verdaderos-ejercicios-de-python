62f382c15bbeacc472ddeff002f1e2a240906e54
jpinzon18 <d345jpinzon@gmail.com>
2023-03-01 08:00:31 -0500

    correccion del 1 marzo

--- 2023/febrero/programacionorientadaaobjetos/modificacion.py
+++ 2023/febrero/programacionorientadaaobjetos/modificacion.py
@@ -10,22 +10,21 @@ class Persona:
     def setNombre(self,nombre):
         self.__nombre=nombre
     
-    def getnumber(self,number):
+    def getnumber(self):
         return self.__number
     
     def setnumber(self,number):
         self.__number=number
 
 
-ob=Persona('Maria')
+ob=Persona('Maria',3647364)
 print(ob.getNombre())
 ob.setNombre('Ana')
 print(ob.getNombre())
-ob.setnumber(345546)
 print(ob.getnumber())
 print(type(ob))
 
-class Aprendiz(Persona):
+'''class Aprendiz(Persona):
         Persona.__init__(self,nombre)
         self.__ficha=ficha
 
@@ -34,4 +33,4 @@ class Aprendiz(Persona):
         return self.__ficha
 
 app=Aprendiz('Pedro',12345)
-print(app.getNombre())
+print(app.getNombre())'''
